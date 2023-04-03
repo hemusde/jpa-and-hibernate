4fc3a4681a500728d64610ac49d17438aaaf01b8
hemusde <hemanth.sde@gmail.com>
2023-04-03 23:13:31 +0530

    Add insert and delete methods

--- src/main/java/com/hemu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/hemu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.hemu.springboot.learnjpaandhibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
@@ -12,8 +14,11 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.inset();
+		repository.insert(new Course(1, "Learn AWS Now!", "hemu"));
+		repository.insert(new Course(2, "Learn Azure Now!", "hemu"));
+		repository.insert(new Course(3, "Learn Devops Now!", "hemu"));
 
+		repository.deleteById(1);
 	}
 
 }
--- src/main/java/com/hemu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/hemu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.hemu.springboot.learnjpaandhibernate.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
 
@@ -12,12 +14,21 @@ public class CourseJdbcRepository {
 
 	private static String INSERT_QUERY = """
 			insert into course (id, name, author)
-			values (1,'Learn AWS', 'in28minutes')
+			values (?,?,?)
 
 			""";
 
-	public void inset() {
-		springJdbcTemplate.update(INSERT_QUERY);
+	public void insert(Course course) {
+		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+
+	}
+
+	private static String DELETE_QUERY = """
+			delete from course where id=?
+			""";
+
+	public void deleteById(long id) {
+		springJdbcTemplate.update(DELETE_QUERY, id);
 
 	}
 
