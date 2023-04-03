b3f5203a676e25a79f5ac639a3babed7a9a2cd3a
hemusde <hemanth.sde@gmail.com>
2023-04-04 01:27:03 +0530

    Add Select Query

--- src/main/java/com/hemu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/hemu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,7 @@
 package com.hemu.springboot.learnjpaandhibernate.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -29,6 +30,15 @@ public class CourseJdbcRepository {
 
 	public void deleteById(long id) {
 		springJdbcTemplate.update(DELETE_QUERY, id);
+
+	}
+
+	private static String SELECT_QUERY = """
+			select * from course where id=?
+			""";
+
+	public Course findById(long id) {
+		return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
 
 	}
 
